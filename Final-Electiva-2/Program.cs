a39c26b52af17f46c3f5f9868e1441af5320b13f
Jese74 <144914044+Jese74@users.noreply.github.com>
2025-08-24 21:16:41 -0400

    Delete Final-Electiva-2 directory

--- Final-Electiva-2/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hola Mundo. Que se cuentan?.");
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-app.Urls.Add($"http://*:{port}");
-
-app.Run();  
